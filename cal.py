641697aa1a27327ec729f9a487fb5a9c20927824
Vk_Gautam_8756 <152972524+Vish875695269@users.noreply.github.com>
2024-08-30 20:48:36 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,18 @@
+print('''
++ ADD
+- SUBSTRACT
+* MULTIPLY     
+- DIVIDE''')
+num1=int(input("Enter the value1:-"))
+num2=int(input("Enter the value2:-")) 
+opr=input("Enter the opr...(+, -,*,/)")
+if opr=="+":
+       print(num1+num2)  
+elif opr=="-":
+      print(num1-num2)
+elif opr=="*":
+      print(num1*num2)
+elif opr=="/":
+      print(num1/num2)
+else: 
+      print("invalid opr...")
